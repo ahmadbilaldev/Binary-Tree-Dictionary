2a490757c425c75711fab38a40a91ae8363bf284
Ghulam Ghous <Itsghulamghous@gmail.com>
2019-12-13 08:58:43 +0500

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -9,25 +9,26 @@ using namespace std;
 
 class Tree
 {
-	Node* root;
-	int count;
+	Node* root; //Root of the tree
+	int count; //Private data member to record count
 
 public:
-	Tree();
-	Tree(Node*& ptr);
-	bool isEmpty();
-	bool isNotEmpty();
-	Tree& insert(string word, string synonym, string defination);
-	void editWord(string existWord, string newWord);
-	Node* removeWord(string word);
-	void printInfixOrder();
-	void setBookMark(string s1);
-	void printMarked();
-	Node* search(string s1);
-	void writeToFile(ofstream& outFile);
-	void readFromFile(ifstream& inFile);
-	bool logIn(ifstream& InFile);
-	void showSynonymAndDefinition(string word);
-	void wordOfTheDay();
-	~Tree();
+	Tree(); //default construtor
+	Tree(Node*& ptr); //parameterized constructor
+	bool isEmpty(); //To check if the tree is empty
+	bool isNotEmpty(); //To Check if the tree is not empty
+	Tree& insert(string word, string synonym, string defination); //To insert a new word in dictionary
+	void editWord(string existWord, string newWord); //To edit a existing word in  dictionary
+	void editSynonym(string word, string newSynonym); //to edit synonym
+	Node* removeWord(string word); //To remove a word from dictionary
+	void printInfixOrder(); //To display dictionary
+	void setBookMark(string s1); //to set bookmarks
+	void printMarked(); //To print book marks
+	Node* search(string s1); //To search a word in the dictionary
+	void writeToFile(ofstream& outFile); //To write in the file
+	void readFromFile(ifstream& inFile); //To read from file
+	bool logIn(ifstream& InFile); //To login
+	void showSynonymAndDefinition(string word); //To show synonyms and definition of a word
+	void wordOfTheDay(); //to show word of the day
+	~Tree(); //destructor
 };
